9d29f30a14e9a1bcc6b6a1e7e97f5bf84fa8042e
Ravi <ravikumar16696@gmail.com>
2023-03-21 06:04:42 +0530

    autowiring using xml

---
+++ src/main/java/com/autowiring/App.java
@@ -0,0 +1,13 @@
+package com.autowiring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("file:/Users/ravi/Documents/TestProjects/SpringCore/springcore/src/main/java/com/autowiring/config.xml");
+        Employee employee = (Employee) context.getBean("employee");
+        Employee employee1 = context.getBean("employee", Employee.class); //here we don't have to typecast
+        System.out.println(employee);
+    }
+}
