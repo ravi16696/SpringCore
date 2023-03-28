aef4f3182ab74c50ac3a41bfbd25deae8e2e3d34
Ravi <ravikumar16696@gmail.com>
2023-03-28 10:13:11 +0530

    spring expression langauage

--- src/main/java/com/spel/Demo.java
+++ src/main/java/com/spel/Demo.java
@@ -6,9 +6,26 @@ import org.springframework.stereotype.Component;
 @Component
 public class Demo {
 
-    @Value("#{4+5}")
+//    @Value("#{4+5}")
+    //@Value("#{T(com.spel.Demo).getValue()}") //note - give the complete package name
+    @Value("#{T(java.lang.Math).sqrt(16)}")
     private Integer x;
 
+    @Value("#{new String('ravi')}")
+    private String str;
+
+    @Value("#{4 < 5}") // it will resolve to false
+    private boolean isActive;
+
+
+
+    public String getStr() {
+        return str;
+    }
+
+    public void setStr(String str) {
+        this.str = str;
+    }
 
     @Value("#{10>5 ? 2 : 8}")
     private Integer y;
@@ -21,22 +38,24 @@ public class Demo {
         return 10;
     }
 
-    public void setX(Integer x) {
-        this.x = x;
-    }
+//    public void setX(Integer x) {
+//        this.x = x;
+//    }
 
     public Integer getY() {
         return y;
     }
-
-    public void setY(Integer y) {
-        this.y = y;
-    }
+//setter is not mandatory to write spring take it by default also
+//    public void setY(Integer y) {
+//        this.y = y;
+//    }
 
     @Override
     public String toString() {
         return "Demo{" +
                 "x=" + x +
+                ", str='" + str + '\'' +
+                ", isActive=" + isActive +
                 ", y=" + y +
                 '}';
     }
